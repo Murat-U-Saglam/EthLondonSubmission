32c6c09a7b1c4c483c5e812f826950165d5b4ee3
rere <regis-graptin@sfr.fr>
2024-03-16 23:15:23 +0100

    front: deploy contract on the front

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,11 +9,6 @@
     <div>
 
       <p>
-        Contract address: 
-        <input v-model="contractAddress">
-      </p>
-
-      <p>
         User 1 address: 
         <input v-model="user_1">
       </p>
@@ -29,6 +24,12 @@
 
     </div>
 
+    <div v-if="contractAddress">
+      <p>
+        Contract deployed: {{ contractAddress }}
+      </p>
+    </div>
+
 
   </div>
 </template>
@@ -36,26 +37,62 @@
 
 <script>
 
+// import { ethers } from "ethers";
+// import { FhenixClient, EncryptionTypes } from 'fhenixjs';
+
+// import hre from "hardhat";
+
+import { BrowserProvider } from "ethers";
+// const { fhenixjs, ethers } = hre;
+// const { deploy } = hre.deployments;
+
+// // Get the provider & define the clent
+// const provider = new BrowserProvider(window.ethereum);
+// fhenixClient = new FhenixClient({provider});
+
+
+// import { ethers } from "ethers";
+// import { FhenixClient, EncryptionTypes } from 'fhenixjs';
+// import { BrowserProvider } from "ethers";
+
+import rawContract from "./ABI.json";
+
+import { ContractFactory } from 'ethers';
+
 export default {
   data() {
     return {
-      contractAddress: "",
+      contractAddress: null,
       user_1: "",
       user_2: "",
     };
   },
   methods: {
-    run_game() {
-      if (
-        this.contractAddress === "" ||
-        this.user_1 === "" ||
-        this.user_2 === ""
-      ) { 
-        console.log("Check the parameter...")
-        return ; 
-      }
+    async run_game() {
+      // if (
+      //   this.user_1 === "" ||
+      //   this.user_2 === ""
+      // ) { 
+      //   console.log("Check the parameter...")
+      //   return ; 
+      // }
+      const provider = new BrowserProvider(window.ethereum);
+      let signer = await provider.getSigner();
 
-      // TODO :: Run the game
+      const factory = new ContractFactory(rawContract.abi, rawContract.bytecode, signer);
+
+      // If your contract requires constructor args, you can specify them here
+        let contract = await factory.deploy(
+          "0x2B8D536768163cBCd2cC5D91D4b03B913244A784", 
+          "0x7a47A0B8Fb2F1e56Db819e186B1A38e95B3c7d3B"
+        );
+
+        console.log("Contract:");
+        console.log(contract);
+
+        await contract.waitForDeployment;
+
+        this.contractAddress = contract.target;
 
     }
   }
