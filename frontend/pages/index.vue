2cea1295ff7987da860f60ccaaee68ed0868a437
rere <regis-graptin@sfr.fr>
2024-03-16 18:54:04 +0100

    update index to have parameter definition

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,43 +1,63 @@
 <template>
+
+  <Header />
+
   <div class="main">
+
+    <h2>Battle game</h2>
+
     <div>
-      <img class="logo" :class="!dark ? 'light' : ''" src="~/assets/images/logo.png" />
+
+      <p>
+        Contract address: 
+        <input v-model="contractAddress">
+      </p>
+
+      <p>
+        User 1 address: 
+        <input v-model="user_1">
+      </p>
+
+      <p>
+        User 2 address: 
+        <input v-model="user_2">
+      </p>
+      
+      <button @click="run_game">
+        Run the game  
+      </button>
+
     </div>
-    <button class="btn btn-success" @click="fnxConnect" :disabled="isItFhenixNetwork">{{ (isItFhenixNetwork) ? 'Connected to Fhenix' : 'Connect to Fhenix Network' }}</button>
-    <div class="address"><b>Address:</b> {{ isItFhenixNetwork ? address : '---' }}</div>
-    <div><b>Balance:</b> {{ isItFhenixNetwork ? balance : '---' }}</div>
-    <div v-if="isItFhenixNetwork">
-      <NuxtLink to="/extra-page">Continue to page 2</NuxtLink>
-    </div>
-    <button class="btn btn-primary btn-sm" @click="toggleTheme()">Switch to {{  dark ? "light" : "dark" }} mode</button>
+
+
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue';
 
-const { dark, toggleTheme } = useThemeToggle();
-const { fnxConnect, isItFhenixNetwork, balance, address } = useChain();
+<script>
 
-onMounted(async () => {
-  if (localStorage.getItem("isConnected")) {
-    if (typeof window.ethereum !== 'undefined') {
-      try {
-        await fnxConnect();
-      } catch (err) {
-        console.error(err);
+export default {
+  data() {
+    return {
+      contractAddress: "",
+      user_1: "",
+      user_2: "",
+    };
+  },
+  methods: {
+    run_game() {
+      if (
+        this.contractAddress === "" ||
+        this.user_1 === "" ||
+        this.user_2 === ""
+      ) { 
+        console.log("Check the parameter...")
+        return ; 
       }
+
+      // TODO :: Run the game
+
     }
   }
-});
+};
 </script>
-
-<style scoped>
-  .logo {
-    height: 40px;
-  }
-
-  .logo.light {
-    filter: invert(100%);
-  }
-</style>
